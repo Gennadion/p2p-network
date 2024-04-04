118b2f9d3ee58d65273fb3024167948154b4199f
Matvey Firsov <151728617+Fin1k9@users.noreply.github.com>
2024-04-04 12:49:53 +0300

    Changed Peer Indexer Logic
    Changed the logic of PeerIndexManager

--- file-management/Indexer.py
+++ file-management/Indexer.py
@@ -4,7 +4,7 @@ import json
 import time
 from Overwatcher import DirectoryMonitor
 
-class LocalFileIndexer:
+class LocalIndexManger:
     def __init__(self, shared_folder, index_file_name='index.json'):
         self.shared_folder = shared_folder
         self.index_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), index_file_name)
@@ -54,59 +54,55 @@ class LocalFileIndexer:
     def delete_index_from_json(self, file_path):
         filename = os.path.basename(file_path)
         
-        file_hash = None
+        file_hash_to_delete = None
         for file_hash, meta_data in self.file_hashes.items():
             if meta_data['name'] == filename:
-                file_hash = file_hash
+                file_hash_to_delete = file_hash
                 break
         
         if file_hash and file_hash in self.file_hashes:
-            del self.file_hashes[file_hash]
+            del self.file_hashes[file_hash_to_delete]
             self.save_index_to_json()
 
     def get_index(self):
         return self.file_hashes
 
 class PeerIndexManager:
-    def __init__(self, index_file='peer_index.json'):
-        self.index_file = index_file #Locally stored index file of peers combined
-        self.peer_file_index = self.load_index_from_file() #Loaded dictionary of file hashes and their metadata
+    def __init__(self, peer_index_file_name='peer_index.json'):
+        self.index_file = peer_index_file_name
+        self.peer_file_index = self.load_peer_index()
 
-    def load_index_from_file(self):
-        """Load the peer file index from a JSON file."""
-        if os.path.exists(self.index_file):
-            with open(self.index_file, 'r') as file:
-                return json.load(file)
-        else:   
-            return {}
+    def load_peer_index(self):
+        try:
+            with open(self.index_file, 'r') as f:
+                return json.load(f)
+        except FileNotFoundError:
+                return {}
 
-    def save_index_to_file(self):
-        """Save the peer file index to a JSON file."""
-        with open(self.index_file, 'w') as file:
-            json.dump(self.peer_file_index, file, indent=4)
+    def save_peer_index(self):
+            with open(self.peer_index_file, 'w') as file:
+                json.dump(self.peer_file_index, file, indent=4)
 
-def update_from_received_index(self, received_index_json, peer_address):
-    """Update the local index from a received JSON index from a peer."""
-    received_index = json.loads(received_index_json)
-    
-    for file_hash, file_attrs in received_index.items():
-        file_metadata = {
-            "name": file_attrs["name"],
-            "size": file_attrs["size"],
-        }
-        if file_hash not in self.peer_file_index: #if hash is not in the peer file, we create a new entry with hash as key 
+    def add_file_from_peer_index(self, file_hash, file_metadata, peer_address):
+        """Add a new file to the index or update existing file metadata."""
+        if file_hash not in self.peer_file_index:
             self.peer_file_index[file_hash] = {
                 "metadata": file_metadata,
                 "peers": {}
             }
-        else:
-            # If the hash is present we just update a peer dictionary with new pair of peer address and last update time
-            self.peer_file_index[file_hash]["peers"][peer_address] = {
-                "last_update": time.time(),
-            }
-    self.save_index_to_file()
+        self.peer_file_index[file_hash]["peers"][peer_address] = {
+            "last_update": time.time()
+        }
+        self.save_peer_index()
 
-
+    def remove_file_from_peer_index(self, file_hash, peer_address):
+        if file_hash in self.peer_file_index:
+            if peer_address in self.peer_file_index[file_hash]["peers"]:
+                del self.peer_file_index[file_hash]["peers"][peer_address]
+                if not self.peer_file_index[file_hash]["peers"]:
+                    del self.peer_file_index[file_hash]
+                self.save_peer_index()
+                
     def get_file_peers(self, file_hash):
         """Get the peers that have the file with the given hash."""
         return self.peer_file_index[file_hash]["peers"] #return the dictionary of peers with last update time
@@ -116,7 +112,7 @@ def update_from_received_index(self, received_index_json, peer_address):
 
 if __name__ == "__main__":
     shared_folder_mac = r'/Users/finik/Desktop/FilesToTransfer'
-    file_indexer = LocalFileIndexer(shared_folder_mac)
+    file_indexer = LocalIndexManger(shared_folder_mac)
     file_indexer.index_files()
     print(f"Indexing complete. Data saved in {file_indexer.index_files}.")
     time.sleep(5)
